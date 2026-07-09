b71f20d67e7b0ba500b00bedf57591f53e7459b0
lty <1392309506@qq.com>
2026-07-09 20:43:55 +0800

    chore: remove temp script

--- do_fix.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-r = subprocess.run(['git', 'add', '-A'], capture_output=True)
-r = subprocess.run(['git', 'commit', '-m', 'fix: run_simulation.py typo + NLV calc'], capture_output=True)
-print(r.stdout.decode()[:300])
-r = subprocess.run(['git', 'push'], capture_output=True)
-print(r.stdout.decode()[:300])
