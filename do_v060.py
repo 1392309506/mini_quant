03d4a89abfb270acc79d5ee50ea3c7388c7d2922
lty <1392309506@qq.com>
2026-07-09 21:32:55 +0800

    chore: remove temp script

--- do_v060.py
+++
@@ -1,6 +0,0 @@
-import subprocess as s
-r = s.run(['git','add','-A'], capture_output=True)
-r = s.run(['git','commit','-m','docs: v0.6.0 文档同步 — TO_DO_LIST/CHANGELOG/README/CONSTITUTION/版本号'], capture_output=True)
-print(r.stdout.decode()[:300])
-r = s.run(['git','push'], capture_output=True)
-print(r.stdout.decode()[:300])
