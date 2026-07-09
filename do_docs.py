1ed00a2fb43ea6d52efe6f9788e7177d9f21cf62
lty <1392309506@qq.com>
2026-07-09 21:15:42 +0800

    chore: remove temp script

--- do_docs.py
+++
@@ -1,6 +0,0 @@
-import subprocess as s
-r = s.run(['git', 'add', '-A'], capture_output=True)
-r = s.run(['git', 'commit', '-m', 'docs: 全面重写使用指南.md — 分类命令、删除Jupyter、补充模拟盘'], capture_output=True)
-print(r.stdout.decode()[:200])
-r = s.run(['git', 'push'], capture_output=True)
-print(r.stdout.decode()[:200])
