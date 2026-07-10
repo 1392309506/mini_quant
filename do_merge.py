46fe6bb6e23215740832f07838702b4bf4c6aa47
lty <1392309506@qq.com>
2026-07-10 20:19:08 +0800

    chore: remove temp script

--- do_merge.py
+++
@@ -1,5 +0,0 @@
-import subprocess as s
-s.run(['git','add','-A'], capture_output=True)
-r = s.run(['git','commit','-m','docs: merge 当前记录 进 审查报告，删除旧文件，更新引用'], capture_output=True)
-print(r.stdout.decode()[:200])
-s.run(['git','push'])
